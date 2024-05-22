e26b9b99b6d62b452e00ad72b344fea32d6cb53d
LabUser <LabUser@DESKTOP-BT4131D>
2024-05-22 22:20:32 +0000

    creates login translation functionality

--- c969/Login.cs
+++ c969/Login.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,20 @@ namespace c969
 {
     public partial class Login : Form
     {
+        public string errorMessage = "Invalid credentials.";
         public Login()
         {
             InitializeComponent();
+
+            //Translate login text to French when localization is changed.
+            if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "en")
+            {
+                usernameLabel.Text = "Nom d'utilisateur";
+                passwordLabel.Text = "Mot de passe";
+                loginButton.Text = "Entrer";
+                this.Text = "Se connecter";
+                errorMessage = "Les informations d'identification invalides.";
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,7 +38,7 @@ namespace c969
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show(errorMessage);
         }
     }
 }
