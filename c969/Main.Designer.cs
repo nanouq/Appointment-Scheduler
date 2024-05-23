f9165333658891b1e783670702831798f6ec0bbc
LabUser <LabUser@DESKTOP-BT4131D>
2024-05-23 23:48:38 +0000

    creates preliminary scheduler GUI

--- c969/Main.Designer.cs
+++ c969/Main.Designer.cs
@@ -28,21 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.appointmentButton = new System.Windows.Forms.Button();
+            this.customerButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(164, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(823, 417);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // appointmentButton
+            // 
+            this.appointmentButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.appointmentButton.Enabled = false;
+            this.appointmentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appointmentButton.Location = new System.Drawing.Point(12, 81);
+            this.appointmentButton.Name = "appointmentButton";
+            this.appointmentButton.Size = new System.Drawing.Size(146, 46);
+            this.appointmentButton.TabIndex = 1;
+            this.appointmentButton.Text = "Appointments";
+            this.appointmentButton.UseVisualStyleBackColor = false;
+            // 
+            // customerButton
+            // 
+            this.customerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerButton.Location = new System.Drawing.Point(12, 133);
+            this.customerButton.Name = "customerButton";
+            this.customerButton.Size = new System.Drawing.Size(146, 46);
+            this.customerButton.TabIndex = 2;
+            this.customerButton.Text = "Customers";
+            this.customerButton.UseVisualStyleBackColor = true;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 345);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(146, 39);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(12, 390);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(146, 39);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(12, 435);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(146, 39);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Appointments";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "View:";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(905, 484);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(82, 25);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(999, 517);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.customerButton);
+            this.Controls.Add(this.appointmentButton);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Main";
             this.Text = "Main";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button appointmentButton;
+        private System.Windows.Forms.Button customerButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button exitButton;
     }
 }
