d8e3e305c67b5346749b8a39d8ba75c46c72eaa0
LabUser <LabUser@DESKTOP-BT4131D>
2024-06-04 18:10:45 +0000

    creates customer class

---
+++ c969/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c969
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public int AddressID { get; set; }
+        public bool Active { get; set; }
+        public string CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastUpdate { get; set; }
+        public string LastUpdatedBy { get; set; }
+
+        public Customer(int customerID, string customerName, int addressID, bool active, string createDate, string createdBy, string lastUpdate, string lastUpdatedBy)
+        {
+            CustomerID = customerID;
+            CustomerName = customerName;
+            AddressID = addressID;
+            Active = active;
+            CreateDate = createDate;
+            CreatedBy = createdBy;
+            LastUpdate = lastUpdate;
+            LastUpdatedBy = lastUpdatedBy;
+
+        }
+    }
+}
