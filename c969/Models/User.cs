d73d3580d2285693270d875892bb267e95d79a2b
LabUser <LabUser@DESKTOP-BT4131D>
2024-07-06 22:45:38 +0000

    adds user class

---
+++ c969/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c969.Models
+{
+    public class User
+    {
+        public int userId {  get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool active { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string Username, string Password)
+        {
+            username = Username;
+            password = Password;
+            active = true;
+        }
+    }
+}
