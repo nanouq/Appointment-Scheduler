06e8f0681582c536c0c3aebc52211c77381515c2
LabUser <LabUser@DESKTOP-BT4131D>
2024-07-14 00:12:57 +0000

    creates a createCountry method

--- c969/AddCustomer.cs
+++ c969/AddCustomer.cs
@@ -47,7 +47,6 @@ namespace c969
             //regex that specifies the phone number field should only be formatted as 0000000000 or 000-000-0000
             Regex regex = new Regex(@"^\d{3}-?\d{3}-?\d{4}$");
             Match match = regex.Match(numberBox.Text.Trim());
-
             //1st check
             //check if all fields are filled out
             if (string.IsNullOrEmpty(firstNameBox.Text.Trim()) || string.IsNullOrEmpty(lastNameBox.Text.Trim()) || string.IsNullOrEmpty(addressBox.Text.Trim()) || string.IsNullOrEmpty(cityBox.Text.Trim()) ||
@@ -61,6 +60,15 @@ namespace c969
             }
             else
             {
+                try
+                {
+                    int countryid = Helper.createCountry(countryBox.Text.Trim(), currentUser.username);
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                
                 MessageBox.Show("Success!");
             }
         }
--- c969/Helper.cs
+++ c969/Helper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using c969.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,59 @@ namespace c969
 {
     public class Helper
     {
+        //creates a country in database or returns ID of existing country
+        public static int createCountry(string countryName, string username)
+        {
+            //check if the country already exists
+            int countryId = 0;
+            string query = $"SELECT countryId FROM country WHERE country = '{countryName}'";
+            MySqlCommand cmd = new MySqlCommand(query, Database.DBConnection.conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                countryId = reader.GetInt32(0);
+                reader.Close();
+            }
+            reader.Close();
 
+            if (countryId == 0) //the country doesnt exist yet
+            {
+                countryId = getNextID("countryId", "country") + 1;
+                
+                string insertQuery = $"INSERT into country (countryId, country, createDate, createdBy, lastUpdate, lastUpdateBy) " +
+                    $"VALUES ('{countryId}', '{countryName}', CURRENT_TIMESTAMP, '{username}', CURRENT_TIMESTAMP, '{username}')";
+
+                MySqlCommand insert = new MySqlCommand(insertQuery, Database.DBConnection.conn);
+  
+                insert.ExecuteNonQuery();
+                return countryId;
+            }
+
+            return countryId;
+        }
+
+        public static int getNextID(string itemID, string table)
+        {
+            int nextId;
+            string query = $"SELECT max({itemID}) FROM {table}";
+            MySqlCommand cmd = new MySqlCommand(query, Database.DBConnection.conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                if (reader[0] == DBNull.Value)
+                {
+                    reader.Close();
+                    return 0;
+                }
+                nextId = reader.GetInt32(0);
+                reader.Close();
+                return nextId;
+            }
+            reader.Close();
+            return 0;
+        }
     }
 }
